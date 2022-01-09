8691d977fa9989eaf45cb2536811132c9b940598
MohamedSalemMf <mohamedsalemfff@gmail.com>
2022-01-09 11:51:31 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -13,6 +13,10 @@
 // ---------------------------------------------------------
 $(document).ready(function(){
     $('.carousel').carousel();
+	setInterval(function(){
+		$('.carousel').carousel('next');
+		
+	},2000);
   });
 
 
